d1a3996d84609163a1036dc7b256229909be7ed0
Deepak Yadav <dy5055290@gmail.com>
2025-01-25 22:57:54 +0530

    Time: 8 ms (32.18%), Space: 43.5 MB (24.6%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
@@ -1,30 +1,48 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        Stack<Character> st = new Stack<>();
-        int open=1;
-        int close=0;
-        int start = 0;
-        for (int i = 1; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '(') {
-                open++;
-            } else {
-                close++;
-            }
-            st.push(c);
-            if(close==open){
-                st.pop();
-                close=0;
-                open=1;
-                i++;
-            }
-            }
-            String d="";
-        while(!st.isEmpty()) {
-        	d=st.pop()+d;
+        // Stack<Character> st = new Stack<>();
+        // int open=1;
+        // int close=0;
+        // int start = 0;
+        // for (int i = 1; i < s.length(); i++) {
+        //     char c = s.charAt(i);
+        //     if (c == '(') {
+        //         open++;
+        //     } else {
+        //         close++;
+        //     }
+        //     st.push(c);
+        //     if(close==open){
+        //         st.pop();
+        //         close=0;
+        //         open=1;
+        //         i++;
+        //     }
+        //     }
+        //     String d="";
+        // while(!st.isEmpty()) {
+        // 	d=st.pop()+d;
             
+        // }
+        // return d;
+        StringBuilder str=new StringBuilder();
+        List<String> ls=new ArrayList<>();
+        int start=0,balance=0;
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c=='(')
+            balance++;
+            if(c==')')
+            balance--;
+            str.append(c);
+            if(balance==0){
+            ls.add(str.toString());
+            str.setLength(0);
+            }
         }
-        return d;
-        
+        StringBuilder result=new StringBuilder();
+        for (String primitive:ls)
+        result.append(primitive.substring(1, primitive.length() - 1));
+        return result.toString();
     }
 }
