a3dce0f40d76e50a32e17b3ba2c6b29dfe1e7a5b
Deepak Yadav <dy5055290@gmail.com>
2025-01-30 22:46:58 +0530

    Time: 4 ms (69.98%), Space: 44.9 MB (52.85%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -8,15 +8,14 @@ class Solution {
         romanMap.put('C',100);
         romanMap.put('D',500);
         romanMap.put('M',1000);
-        int sum=0,prev=1000;
-        for(int i=0;i<s.length();i++){
+        int sum=0,prev=0;
+        for(int i=s.length()-1;i>=0;i--){
             int curr=romanMap.get(s.charAt(i));
-            if(curr>prev){
-            curr=curr-prev*2;
-            sum=sum+curr;
+            if(curr<prev){
+            sum=sum-curr;
             }
             else{
-              sum=sum+curr; 
+            sum=sum+curr; 
             }
             prev=curr;
         }
