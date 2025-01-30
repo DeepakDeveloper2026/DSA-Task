f03fd44d677112c38e9453e2f2d4ad135e7ea01a
Deepak Yadav <dy5055290@gmail.com>
2025-01-30 22:43:48 +0530

    Time: 4 ms (69.98%), Space: 44.6 MB (92.01%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,25 +1,24 @@
 class Solution {
     public int romanToInt(String s) {
-        String[] romanArr={"I","V","X","L","C","D","M"};
-        int[]  numArr={1,5,10,50,100,500,1000};
-        int sum=0,curr=0,prev=1000;
-        for(int j=0;j<s.length();j++){
-            for(int i=0;i<romanArr.length;i++){
-                if(String.valueOf(s.charAt(j)).equals(romanArr[i])){
-                    curr=numArr[i];
-                   if(curr>prev){
-                    curr=curr-prev*2;
-                    sum=sum+curr;
-                    
-                   }
-                   else{
-                      sum=sum+curr;
-                      
-                   }
-                   prev=curr;
-                }
+        HashMap<Character,Integer> romanMap=new HashMap<>();
+        romanMap.put('I',1);
+        romanMap.put('V',5);
+        romanMap.put('X',10);
+        romanMap.put('L',50);
+        romanMap.put('C',100);
+        romanMap.put('D',500);
+        romanMap.put('M',1000);
+        int sum=0,prev=1000;
+        for(int i=0;i<s.length();i++){
+            int curr=romanMap.get(s.charAt(i));
+            if(curr>prev){
+            curr=curr-prev*2;
+            sum=sum+curr;
             }
-
+            else{
+              sum=sum+curr; 
+            }
+            prev=curr;
         }
         return sum;
     }
