c0246baf02fa581f782ae07c5b0bf3d5ebef410a
Deepak Yadav <dy5055290@gmail.com>
2025-06-16 11:40:57 +0530

    Time: 1 ms (25.35%), Space: 50.1 MB (90.56%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > max)
+                max = nums[i];
+        }
+        int freq[]=new int[max+1];
+        for(int i=0;i<nums.length;i++){
+            freq[nums[i]]++;
+        }
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]==1)
+            return i;
+        }
+        return -1;
+    }
+}
