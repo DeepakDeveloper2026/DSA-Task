be0e431e2e1fd1cb797c5922c27a0667fc55eb48
Deepak Yadav <dy5055290@gmail.com>
2025-06-19 08:41:40 +0530

    Time: 34 ms (30.59%), Space: 62.1 MB (65.13%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,22 +1,40 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
         if(nums.length==0 || nums.length==1) return nums.length;
-        PriorityQueue<Integer> minheap=new PriorityQueue<>();
+        // PriorityQueue<Integer> minheap=new PriorityQueue<>();
+        // for(int num:nums){
+        //     minheap.offer(num);
+        // }
+        // int max=1;
+        // int cnt=1;
+        // int curr=minheap.poll();
+        // while(!minheap.isEmpty()){
+        //     int next=minheap.poll();
+        //     if(next==curr) continue;
+        //     else if(next==curr+1){
+        //         cnt++;
+        //         max=Math.max(max,cnt);
+        //     }
+        //     else cnt=1;
+        //     curr=next;
+        // }
+        // return max;
+        HashSet<Integer> set=new HashSet<>();
         for(int num:nums){
-            minheap.offer(num);
+            if(!set.contains(num))
+            set.add(num);
         }
-        int max=1;
-        int cnt=1;
-        int curr=minheap.poll();
-        while(!minheap.isEmpty()){
-            int next=minheap.poll();
-            if(next==curr) continue;
-            else if(next==curr+1){
+        int max=0;
+        for(int val:set){
+            if(!set.contains(val-1)){
+                int currNum=val;
+                int cnt=1;
+            while(set.contains(currNum+1)){
+                currNum++;
                 cnt++;
-                max=Math.max(max,cnt);
             }
-            else cnt=1;
-            curr=next;
+            max=Math.max(max,cnt);
+            }
         }
         return max;
     }
